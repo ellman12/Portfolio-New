9d21306a28245cb491cb54ba3e89487a2ab11810
Elliott DuCharme <ellduc4@gmail.com>
2025-02-21 12:19:19 -0600

    Projects can expand without any animations or images

--- src/components/Timeline/SectionTimelineItem.tsx
+++ src/components/Timeline/SectionTimelineItem.tsx
@@ -8,24 +8,25 @@ type Props = {
     endDate: string;
     title: string;
     color: OverridableStringUnion<"primary" | "secondary" | "error" | "warning" | "info" | "success", TimelineDotPropsColorOverrides>;
+    expanded?: boolean;
     lastItem?: boolean;
     children: ReactNode;
 } & Omit<ComponentProps<typeof TimelineDot>, "color">;
 
-const SectionTimelineItem: FC<Props> = ({startDate, endDate, title, lastItem = false, color, children}) => {
+const SectionTimelineItem: FC<Props> = ({startDate, endDate, title, expanded = false, lastItem = false, color, children}) => {
     return (
         <ScrollOnceAnimation>
-            <TimelineItem>
-                <TimelineOppositeContent>
-                    <div className="inline-flex flex-row items-center flex-wrap sm:flex-nowrap">
-                        <span className="text-nowrap">{startDate}—</span>
-                        <span className="text-nowrap">{endDate}</span>
-                    </div>
-                </TimelineOppositeContent>
-                <TimelineSeparator>
+            <TimelineItem className={`${expanded ? "inline" : "flex"}`}>
+                {!expanded && <TimelineOppositeContent>
+					<div className="inline-flex flex-row items-center flex-wrap sm:flex-nowrap">
+						<span className="text-nowrap">{startDate}—</span>
+						<span className="text-nowrap">{endDate}</span>
+					</div>
+				</TimelineOppositeContent>}
+                {!expanded && <TimelineSeparator>
                     <TimelineDot color={color}/>
                     {lastItem ? <></> : <TimelineConnector/>}
-                </TimelineSeparator>
+                </TimelineSeparator>}
                 <TimelineContent>
                     <div className="mb-4">
                         <p className="font-bold">{title}</p>
