c7c5af539e322db4200d82ee1d317e056d579c0e
Elliott DuCharme <ellduc4@gmail.com>
2025-02-22 19:40:30 -0600

    Change wall of text in about me to simple grid with tidbits

--- src/components/Sections/AboutMeSection.tsx
+++ src/components/Sections/AboutMeSection.tsx
@@ -1,27 +1,53 @@
-import PersonIcon from "@mui/icons-material/Person";
+import H3 from "../H3.tsx";
 import Section from "./Section.tsx";
 import Tidbit from "../Tidbit.tsx";
 import ChatIcon from "@mui/icons-material/Chat";
+import WorkIcon from "@mui/icons-material/Work";
 import PlaceIcon from "@mui/icons-material/Place";
 import SchoolIcon from "@mui/icons-material/School";
-import WorkIcon from "@mui/icons-material/Work";
+import PersonIcon from "@mui/icons-material/Person";
+import AdsClickIcon from "@mui/icons-material/AdsClick";
+import OutdoorGrillIcon from '@mui/icons-material/OutdoorGrill';
+import CasinoIcon from '@mui/icons-material/Casino';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import CodeIcon from '@mui/icons-material/Code';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
+import PedalBikeIcon from '@mui/icons-material/PedalBike';
+import HikingIcon from '@mui/icons-material/Hiking';
 import ScrollOnceAnimation from "../ScrollOnceAnimation.tsx";
 
 export default function AboutMeSection() {
     return (
         <Section icon={<PersonIcon/>} title="About Me">
-            <ScrollOnceAnimation>
-                <div className="grid grid-cols-2 gap-4 sm:px-8">
-                    <Tidbit icon={<WorkIcon/>} text="KBR — Software Engineer"/>
-                    <Tidbit icon={<PlaceIcon/>} text="Sioux Falls, SD"/>
-                    <Tidbit icon={<SchoolIcon/>} text="Dakota State University"/>
-                    <Tidbit icon={<ChatIcon/>} text="He/him"/>
-                </div>
-            </ScrollOnceAnimation>
+            <div className="flex flex-col gap-4">
+                <ScrollOnceAnimation>
+                    <div className="grid grid-cols-2 gap-4 sm:px-8 mb-4">
+                        <Tidbit icon={<WorkIcon/>} text="KBR — Software Engineer"/>
+                        <Tidbit icon={<PlaceIcon/>} text="Sioux Falls, SD"/>
+                        <Tidbit icon={<SchoolIcon/>} text="Computer Science, Dakota State University"/>
+                        <Tidbit icon={<ChatIcon/>} text="He/him"/>
+                    </div>
+                </ScrollOnceAnimation>
 
-            <ScrollOnceAnimation>
-                <p className="mt-8 mx-2 md:mx-8">My name is Elliott DuCharme. I have been working as a software engineer for KBR in Sioux Falls, SD since June 2023. Previously, I interned as a software engineer at Daktronics. I earned my B.S. in Computer Science from Dakota State University, where I studied from August 2020 to May 2023. I was the lead programmer on my high school's FRC team, which led to my love of robotics. My hobbies include archery, cooking, D&amp;D, exercise, gaming, and working on personal projects, usually ones that enhance quality of life. Through several projects over the years, I have developed a love for automation, databases, and game design, as well as a love for learning new technologies.</p>
-            </ScrollOnceAnimation>
+                <ScrollOnceAnimation>
+                    <H3>Hobbies</H3>
+
+                    <ScrollOnceAnimation>
+                        <div className="mt-6 grid grid-cols-2 gap-4 sm:px-8 mb-4">
+                            <Tidbit icon={<AdsClickIcon/>} text="Archery"/>
+                            <Tidbit icon={<PedalBikeIcon/>} text="Biking"/>
+                            <Tidbit icon={<OutdoorGrillIcon/>} text="Cooking"/>
+                            <Tidbit icon={<CodeIcon/>} text="Coding"/>
+                            <Tidbit icon={<CasinoIcon/>} text="D&D"/>
+                            <Tidbit icon={<FitnessCenterIcon/>} text="Exercise"/>
+                            <Tidbit icon={<SportsEsportsIcon/>} text="Gaming"/>
+                            <Tidbit icon={<HikingIcon/>} text="Hiking"/>
+                            <Tidbit icon={<PrecisionManufacturingIcon/>} text="Robotics"/>
+                        </div>
+                    </ScrollOnceAnimation>
+                </ScrollOnceAnimation>
+            </div>
         </Section>
     );
 }
