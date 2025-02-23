0c2ff10e45f20e14c126ef1beaa1014f7d835dc9
Elliott DuCharme <ellduc4@gmail.com>
2025-02-22 19:20:26 -0600

    Fix missing scroll animation

--- src/components/Sections/Projects/ProjectsSection.tsx
+++ src/components/Sections/Projects/ProjectsSection.tsx
@@ -3,11 +3,14 @@ import CodeIcon from "@mui/icons-material/Code";
 import Projects from "./Projects.ts";
 import SectionTimeline from "../../Timeline/SectionTimeline.tsx";
 import ProjectDisplay from "./ProjectDisplay.tsx";
+import ScrollOnceAnimation from "../../ScrollOnceAnimation.tsx";
 
 export default function ProjectsSection() {
     return (
         <Section icon={<CodeIcon/>} title="Projects">
-            <p className="mb-6 mx-2 md:mx-8">I've developed {Projects.length} personal projects over the years, all of which I'm very proud of. I love making new projects and learning new things along the way.</p>
+            <ScrollOnceAnimation>
+                <p className="mb-6 mx-2 md:mx-8">I've developed {Projects.length} personal projects over the years, all of which I'm very proud of. I love making new projects and learning new things along the way.</p>
+            </ScrollOnceAnimation>
 
             <SectionTimeline>
                 {Projects.sort((a, b) => a.startDate > b.startDate ? 1 : -1).map((project, index) => (
