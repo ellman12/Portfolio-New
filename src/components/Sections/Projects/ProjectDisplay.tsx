625fb7bf2651939043eccc6e06d11c66ee30ec1f
Elliott DuCharme <ellduc4@gmail.com>
2025-02-22 19:03:52 -0600

    Fix scroll animation with expanded project timeline items

--- src/components/Sections/Projects/ProjectDisplay.tsx
+++ src/components/Sections/Projects/ProjectDisplay.tsx
@@ -3,6 +3,7 @@ import {Project} from "./Projects.ts";
 import SectionTimelineItem from "../../Timeline/SectionTimelineItem.tsx";
 import ProjectCarousel from "./ProjectCarousel.tsx";
 import H3 from "../../H3.tsx";
+import ScrollOnceAnimation from "../../ScrollOnceAnimation.tsx";
 
 interface Props {
     project: Project;
@@ -14,14 +15,16 @@ const ProjectDisplay: FC<Props> = ({project, lastItem}) => {
 
     return (
         <div onClick={() => setExpanded(!expanded)}>
-            {!expanded && <SectionTimelineItem startDate={project.startDate.toLocaleDateString()} endDate={project.endDate?.toLocaleDateString() ?? "Present"} title={project.name} color="primary" lastItem={lastItem} expanded={expanded}>
-				<div className={`flex flex-col gap-4`}>
-					<div dangerouslySetInnerHTML={{__html: project.briefDesc}}/>
-				</div>
-			</SectionTimelineItem>}
+            <ScrollOnceAnimation>
+                {!expanded && <SectionTimelineItem startDate={project.startDate.toLocaleDateString()} endDate={project.endDate?.toLocaleDateString() ?? "Present"} title={project.name} color="primary" lastItem={lastItem} expanded={expanded}>
+					<div className={`flex flex-col gap-4`}>
+						<div dangerouslySetInnerHTML={{__html: project.briefDesc}}/>
+					</div>
+				</SectionTimelineItem>}
+            </ScrollOnceAnimation>
 
             {expanded && <div className="bg-[#101114] flex flex-col gap-4 my-4 p-4">
-                <H3>{project.name}</H3>
+				<H3>{project.name}</H3>
 
 				<div dangerouslySetInnerHTML={{__html: project.briefDesc}}/>
 
